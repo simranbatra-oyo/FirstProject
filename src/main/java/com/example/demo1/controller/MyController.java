5d39e88f3b00f88847d4488eccb2084154c1b3f9
simranbatra-oyo <simran.batra@oyorooms.com>
2021-02-08 12:58:49 +0530

    abcd

--- src/main/java/com/example/demo1/controller/MyController.java
+++ src/main/java/com/example/demo1/controller/MyController.java
@@ -10,13 +10,10 @@ import com.example.demo1.service.FeedbackService;
 import com.example.demo1.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -85,19 +82,52 @@ public class MyController {
     }
 
 
+//    @PostMapping("/addReview/{hotel_id}/{user_id}")
+//    public String addReviewForHotel(@PathVariable Long user_id, @PathVariable Long hotel_id, @Param("rating") float rating, @Param("review") String review) {
+//        //how to get primary key and add that
+//        Feedback feedback = new Feedback();
+//        //User user = userRepository.findByUser_id(user_id);
+//
+//        User user = userRepository.getOne(user_id);
+//        feedback.setUser(user);
+//        Hotel hotel = hotelRepository.getOne(hotel_id);
+//        feedback.setHotel(hotel);
+//        feedback.setRating(rating);
+//        feedback.setReview(review);
+//        feedbackRepository.save(feedback);
+//        return "Added new review to repo!";
+//    }
     @PostMapping("/addReview/{hotel_id}/{user_id}")
-    public String addReviewForHotel(@PathVariable Long user_id, @PathVariable Long hotel_id, @Param("rating") float rating, @Param("review") String review) {
-        //how to get primary key and add that
-        Feedback feedback = new Feedback();
-        //User user = userRepository.findByUser_id(user_id);
-
-        User user = userRepository.getOne(user_id);
-        feedback.setUser(user);
-        Hotel hotel = hotelRepository.getOne(hotel_id);
-        feedback.setHotel(hotel);
-        feedback.setRating(rating);
-        feedback.setReview(review);
-        feedbackRepository.save(feedback);
-        return "Added new review to repo!";
+    public ResponseEntity<Feedback> addReviewForHotel(@PathVariable Long user_id, @PathVariable Long hotel_id,@RequestBody Feedback feedback){
+        Feedback createFeedback=feedbackRepository.save(feedback);
+        return new ResponseEntity<Feedback>(feedback, HttpStatus.OK);
     }
+
+//    //Edit/Update a Todo
+//    //PUT /users/{user_name}/todos/{todo_id}
+//    @PutMapping("/users/{username}/todos/{id}")
+//    public ResponseEntity<Todo> updateTodo(
+//            @PathVariable String username,
+//            @PathVariable long id, @RequestBody Todo todo){
+//
+//        Todo todoUpdated = todoService.save(todo);
+//
+//        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+//    }
+
+
+//    @PostMapping("/users/{username}/todos")
+//    public ResponseEntity<Void> updateTodo(
+//            @PathVariable String username, @RequestBody Todo todo){
+//
+//        Todo createdTodo = todoService.save(todo);
+//
+//        //Location
+//        //Get current resource url
+//        ///{id}
+//        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+//                .path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+//
+//        return ResponseEntity.created(uri).build();
+//    }
 }
--- src/main/java/com/example/demo1/repository/FeedbackRepository.java
+++ src/main/java/com/example/demo1/repository/FeedbackRepository.java
@@ -13,4 +13,20 @@ public interface FeedbackRepository extends CrudRepository<Feedback, Long>, JpaR
 
     @Query("SELECT f FROM Feedback f WHERE f.hotel = ?1")
     List<Feedback> findRoomByStatus(Long hotelId);
+
+//    public Feedback save(Feedback feedback){
+//        feedback.add(feedback);
+//    }
+
+//    public Todo save(Todo todo) {
+//        if(todo.getId()==-1 || todo.getId()==0) {
+//            todo.setId(++idCounter);
+//            todos.add(todo);
+//        } else {
+//            deleteById(todo.getId());
+//            todos.add(todo);
+//        }
+//        return todo;
+//    }
+
 }
